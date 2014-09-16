8b2820328c42a122619ec0c3070b0998ebbc98d1
Jesus Magana <maganazero5@gmail.com>
2014-09-16 16:27:34 -0700

    Added an array list of earthquakes to the fragment class and an adapter

--- src/com/ZeroLinux5/earthquake/EarthquakeListFragment.java
+++ src/com/ZeroLinux5/earthquake/EarthquakeListFragment.java
@@ -1,6 +1,19 @@
 package com.ZeroLinux5.earthquake;
 
+import java.util.ArrayList;
+
 import android.app.ListFragment;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class EarthquakeListFragment extends ListFragment {
+	ArrayAdapter<Quake> aa;
+	ArrayList<Quake> earthquakes = new ArrayList<Quake>();
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		int layoutID = android.R.layout.simple_list_item_1;
+		aa = new ArrayAdapter<Quake>(getActivity(), layoutID , earthquakes); setListAdapter(aa);
+	}
 }
