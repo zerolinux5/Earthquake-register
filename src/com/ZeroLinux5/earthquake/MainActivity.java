e5d3b19a8bf3e898f4606936bd390e2c127fae37
Jesus Magana <maganazero5@gmail.com>
2014-09-18 19:08:11 -0700

    Added update from preferences method

--- src/com/ZeroLinux5/earthquake/MainActivity.java
+++ src/com/ZeroLinux5/earthquake/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ZeroLinux5.earthquake;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,6 +14,9 @@ public class MainActivity extends Activity {
 	static final private int MENU_PREFERENCES = Menu.FIRST+1; 
 	static final private int MENU_UPDATE = Menu.FIRST+2;
 	private static final int SHOW_PREFERENCES = 1;
+	public int minimumMagnitude = 0;
+	public boolean autoUpdateChecked = false; 
+	public int updateFreq = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +40,25 @@ public class MainActivity extends Activity {
 				return true;
 			} 
 		}
-	return false; }
+		return false; 
+	}
+	
+	private void updateFromPreferences() { 
+		Context context = getApplicationContext(); 
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		int minMagIndex = prefs.getInt(PreferencesActivity.PREF_MIN_MAG_INDEX, 0); 
+		if (minMagIndex < 0)
+			minMagIndex = 0;
+		int freqIndex = prefs.getInt(PreferencesActivity.PREF_UPDATE_FREQ_INDEX, 0); 
+		if (freqIndex < 0)
+			freqIndex = 0;
+		autoUpdateChecked = prefs.getBoolean(PreferencesActivity.PREF_AUTO_UPDATE, false);
+		Resources r = getResources();
+		// Get the option values from the arrays.
+		String[] minMagValues = r.getStringArray(R.array.magnitude); 
+		String[] freqValues = r.getStringArray(R.array.update_freq_values);
+		// Convert the values to ints.
+		minimumMagnitude = Integer.valueOf(minMagValues[minMagIndex]); 
+		updateFreq = Integer.valueOf(freqValues[freqIndex]);
+	}
 }
