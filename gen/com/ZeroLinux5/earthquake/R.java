1b18bd07977fa7b77ecd5af1a56abb6208f535e9
Jesus Magana <maganazero5@gmail.com>
2014-09-18 18:19:02 -0700

    Added preferences and strings files

--- gen/com/ZeroLinux5/earthquake/R.java
+++ gen/com/ZeroLinux5/earthquake/R.java
@@ -1086,7 +1086,9 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int cancelButton=0x7f050041;
         public static final int checkbox=0x7f05002d;
+        public static final int checkbox_auto_update=0x7f05003d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
@@ -1107,6 +1109,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int okButton=0x7f050040;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1124,6 +1127,8 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int spinner_quake_mag=0x7f05003f;
+        public static final int spinner_update_freq=0x7f05003e;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
@@ -1184,7 +1189,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int preferences=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1228,8 +1234,13 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
+        public static final int auto_update_prompt=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
+        public static final int menu_preferences=0x7f0a0014;
+        public static final int menu_update=0x7f0a0010;
+        public static final int min_quake_mag_prompt=0x7f0a0013;
         public static final int quake_feed=0x7f0a000f;
+        public static final int update_freq_prompt=0x7f0a0012;
     }
     public static final class style {
         /** 
